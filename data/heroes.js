36abe93be54529a930fee786ec1918bc196a7312
EMANUEL <emaneduave08@gmail.com>
2026-04-04 11:17:01 +0800

    Create heroes.js

---
+++ data/heroes.js
@@ -0,0 +1,27 @@
+const heroes = [
+  {
+    official: "Miyamoto Musashi",
+    nickname: "Swordmaster",
+    page: "heroes/hok/miyamoto.html"
+  },
+  {
+    official: "Zhang Fei",
+    nickname: "Shield Tank",
+    page: "heroes/hok/zhangfei.html"
+  },
+  {
+    official: "Daji",
+    nickname: "Fox Sorceress",
+    page: "heroes/hok/daji.html"
+  },
+  {
+    official: "Wang Zhaojun",
+    nickname: "Ice Mage",
+    page: "heroes/hok/wangzhaojun.html"
+  },
+  {
+    official: "Gongsun Li",
+    nickname: "Agile Marksman",
+    page: "heroes/hok/gongsunli.html"
+  }
+];
